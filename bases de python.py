0e485683a4dd578624888ac62710f32ceb37bfe7
Adsdworld <128044117+Adsdworld@users.noreply.github.com>
2023-05-08 17:51:19 +0200

    màj rapide

--- bases de python.py
+++ bases de python.py
@@ -14,35 +14,34 @@ while stop: #Tant que True == stop(True) c'est VRAI, Tant que True == stop(False
 prénom = input("entrez votre prénom")
 print("Vous vous appellez {} et vous avez {} ans".format(prénom, age))
 
-print()
-print("***")
+input("Appuyer sur Entrer pour continuer")
+
+print("\n***")
 liste=['Jean', 'alexandre',3,4]
-print("création d'une liste : {}".format(liste))
-print("     for i in range(4)")
-print("         print(liste[i])")
+print("création d'une liste : {}".format(liste)+"\n     for i in range(4)\n         print(liste[i])")
 for i in range(4):  #i va de 0 à range-1 ici 3
     print(liste[i])
 
-print("on prends chaque terme de la liste {} dans i et on vérifie si int(i)==3 ou int(i) == 4".format(liste))
-print("     for i in liste")
-print("         if str(i) == str(3) or str(i) == str(4):")
-print("         i est un nombre, i à pour valeur {}\".format(i)")
+input("Appuyer sur Entrer pour continuer")
+
+print("on prends chaque terme de la liste {} dans i et on vérifie si str(i)==str(3) ou str(i) == str(4)".format(liste)+"\n     for i in liste\n         if str(i) == str(3) or str(i) == str(4):\n         print(\"i est un nombre, i à pour valeur {}\".format(i)\")\n     else:\n         print(\"i n'est pas égal à str(3) ou str(4), i à pour valeur {}\".format(i))")
 for i in liste:  #i va de 0 à la longueur de la liste-1 soit 3
     if str(i) == str(3) or str(i) == str(4):
-        print("i est un nombre, i à pour valeur {}".format(i))
+        print("i est égal à str(3) ou str(4), i à pour valeur {}".format(i))
+    else:
+        print("i n'est pas égal à str(3) ou str(4), i à pour valeur {}".format(i))
 
-print()
-print("***")
+input("Appuyer sur Entrer pour continuer")
+
+print("\n***")
 liste2 = [ 'Cosinus', 'Sinus', 'Tangente', 'Cotangente' ]
 print("création d'une liste : {}".format(liste2))
 liste2.sort()
 print("rangement de la liste avec liste2.sort() : {}".format(liste2))
 
-print("on prends chaque terme de la liste {} dans i et on insert au i ème terme i+\" hyperbolique\"".format(liste2))
-print("une fois l'insertion éffectué on à on supprime le terme de position i")
-print("     for i in liste")
-print("         liste2.insert(i,str(liste2[i]+\" hyperbolique\"))")
-print("         del liste2[i+1]")
+input("Appuyer sur Entrer pour continuer")
+
+print("on prends chaque terme de la liste {} dans i et on insert au i ème terme liste2[i]+\" hyperbolique\"".format(liste2)+"\nune fois l'insertion éffectué on à on supprime le terme de position i\n     for i in liste\n         liste2.insert(i,str(liste2[i]+\" hyperbolique\"))\n         del liste2[i+1]")
 for i in range(len(liste2)):
     print(liste2)
     liste2.insert(i,str(liste2[i]+" hyperbolique"))
@@ -50,11 +49,9 @@ for i in range(len(liste2)):
     del liste2[i+1]
 print(liste2)
 
-print()
-print("***")
-print("on importe la librairie random et on demande un nombre entier randomisé entre 0 inclu et 4 inclu")
-print("     import random ")
-print("     print(random.randint(0,4))")
+input("Appuyer sur Entrer pour continuer")
+
+print("\n***\non importe la librairie random et on demande un nombre entier randomisé entre 0 inclu et 4 inclu\n     import random\n     print(random.randint(0,4))")
 import random 
 print(random.randint(0,4))
     
